114b5d7350aabae4a378dd68e8ffa7c04843371a
ugwustanley <ugwuchiagoziestanley@gmail.com>
2021-10-21 16:15:26 +0100

    removed build

--- build/src/middleware/geneateKey.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var generateApiKey = require('generate-api-key');
-function getApikey() {
-    return generateApiKey({ method: "string" });
-}
-exports.default = getApikey;
